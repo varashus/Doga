ab6e3bd6c5303827e41a4b08a9dec02a64a0060d
varashus <rigodavid100@gmail.com>
2023-10-24 12:06:42 +0200

    Add project files.

---
+++ Doga/Bolygo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doga
+{
+    internal class Bolygo
+    {
+        public Bolygo(string r)
+        {
+            var v = r.Split(';');
+            this.bolygo = v[0];
+            this.holdak = int.Parse(v[1]);
+            this.terfogat = double.Parse(v[2].Replace('.', ','));
+        }
+
+        public string bolygo { get; set; }
+        public int holdak { get; set; }
+        public double terfogat { get; set; }
+    }
+}
---
+++ Doga/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Doga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bolygok = new List<Bolygo>();
+            using StreamReader sr = new StreamReader(
+                path: @"..\..\..\src\solsys.txt",
+                encoding: System.Text.Encoding.UTF8
+                );
+            while (!sr.EndOfStream)
+            {
+                bolygok.Add(new Bolygo(sr.ReadLine()));
+            }
+
+
+
+            //foreach (var x in bolygok)
+            //{
+            //    Console.WriteLine(x.bolygo);
+            //}
+
+            //1
+            Console.WriteLine($"{bolygok.Count()} van a naprendszerben");
+            //2
+            double avarage = bolygok.Average(n => n.holdak);
+            Console.WriteLine($"a naprendszerben egy bolygónak átlagosan {avarage} holdja van");
+            //3
+            var max = bolygok.OrderBy(n => n.terfogat).Last();
+            Console.WriteLine($"A legnagyobb térfogatú bolygó a {max.bolygo}");
+            //4
+            Console.Write("Írd be a keresett bolygó nevét: ");
+            var bolygonev = Console.ReadLine();
+            Console.WriteLine(bolygok.SingleOrDefault(n => n.bolygo == bolygonev) != null ?
+                "Van ilyen nevű bolygó a naprendszerben" :
+                "Sajnos nincs ilyen nevű bolygó a naprendszerben");
+            //5
+            Console.Write("Írj be egy egész számot: ");
+            var egesz = Convert.ToInt32(Console.ReadLine());
+            var holdaktobb = bolygok.Where(b => b.holdak > egesz).Select(b => b.bolygo).ToArray();
+            Console.WriteLine($"A következő bolygoknak van {egesz}-nél/nál több holdja:");
+            foreach (var x in holdaktobb)
+            {
+                Console.Write($"{x},");
+            }
+        }
+    }
+}
